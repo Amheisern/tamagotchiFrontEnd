e46df00aaaf4e667036a7943d904bef6561617e1
lodashreload <icarusjapan@gmail.com>
2022-04-30 12:46:42 -0400

    added delete function untested

--- src/pages/PetDetails.tsx
+++ src/pages/PetDetails.tsx
@@ -1,12 +1,23 @@
 import React from 'react'
-import { useParams } from 'react-router'
+import { useNavigate, useParams } from 'react-router'
+
 
 export function PetDetails() {
-  const params = useParams<{id: number}>()
+  const history = useNavigate()
+  const params = useParams<{id: string}>()
+
+  async function deletePet() {
+    const response = await axios.delete(
+      `https://lodashtamagotchi.herokuapp.com/api/Pets/${params.id}`
+    )
+    if (response.status === 204) {
+      history('/')
+    }
+  }
   return <div>
     <h1>Pet Details</h1>
     <p>
-      Pet ID: {params.id}
+      Pet ID:
       {params.name}
     </p>
   </div>
