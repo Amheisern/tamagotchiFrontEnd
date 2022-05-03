7ee59991c5f4198e0e2fecd29f4124c574b52290
lodashreload <icarusjapan@gmail.com>
2022-05-02 21:56:04 -0400

    added button and display hunger levels and happiness levels

--- src/pages/PetDetails.tsx
+++ src/pages/PetDetails.tsx
@@ -3,20 +3,19 @@ import React, { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router'
 import { PetType } from '../App'
 
-
 export function PetDetails() {
   const history = useNavigate()
-  const params = useParams<{id: string ; actions: 'Playtimes' | 'Feedings' | 'Scoldings'}>()
-  
-  const [petDetails, setPetDetails] = useState<PetType>
-  ({
-  id:  undefined!,
-  name: '',
-  birthday: '',
-  hungerLevel: 0,
-  happinessLevel: 0,
-  lastInteractedWithDate: undefined,
-  isDead: false,
+  const params =
+    useParams<{ id: string; actions: 'Playtimes' | 'Feedings' | 'Scoldings' }>()
+
+  const [petDetails, setPetDetails] = useState<PetType>({
+    id: undefined!,
+    name: '',
+    birthday: '',
+    hungerLevel: 0,
+    happinessLevel: 0,
+    lastInteractedWithDate: undefined,
+    isDead: false,
   })
   const [petHunger, setPetHunger] = useState<PetType>()
   const [petPlay, setPetPlay] = useState<PetType>()
@@ -29,67 +28,66 @@ export function PetDetails() {
       )
       if (response.status === 200) {
         setPetDetails(response.data)
-        console.log(response.data);     
+        console.log(response.data)
       }
     }
-  fetchPetDetails()
-}, [params.id],)
-        async function deletePet() {
-          const response = await axios.delete(
-            `https://lodashtamagotchi.herokuapp.com/api/Pets/${params.id}`
-          )
-          if (response.status === 204) {
-            history('/')
-          }
-        }
-        if (!petDetails.id) {
-          return null
-        }
-
- async function feedPet() {
-  const response = await axios.post(
-    `https://lodashtamagotchi.herokuapp.com/api/Pets/${params.id}/Feedings`
-  )
-  if (response.status === 200) {
-    setPetHunger(response.data)
+    fetchPetDetails()
+  }, [params.id])
+  async function deletePet() {
+    const response = await axios.delete(
+      `https://lodashtamagotchi.herokuapp.com/api/Pets/${params.id}`
+    )
+    if (response.status === 204) {
+      history('/')
+    }
   }
-}
-async function playWithPet() {
-  const response = await axios.post(
-    `https://lodashtamagotchi.herokuapp.com/api/Pets/${params.id}/Playtimes`
-  )
-  if (response.status === 200) {
-    setPetPlay(response.data)
+  if (!petDetails.id) {
+    return null
   }
-}
-async function scoldPet() {
-  const response = await axios.post(
-    `https://lodashtamagotchi.herokuapp.com/api/Pets/${params.id}/Scoldings`
-  )
-  if (response.status === 200) {
-    setPetScold(response.data)
+
+  async function feedPet() {
+    const response = await axios.post(
+      `https://lodashtamagotchi.herokuapp.com/api/Pets/${params.id}/Feedings`
+    )
+    if (response.status === 200) {
+      setPetHunger(response.data)
+    }
   }
-}
+  async function playWithPet() {
+    const response = await axios.post(
+      `https://lodashtamagotchi.herokuapp.com/api/Pets/${params.id}/Playtimes`
+    )
+    if (response.status === 200) {
+      setPetPlay(response.data)
+    }
+  }
+  async function scoldPet() {
+    const response = await axios.post(
+      `https://lodashtamagotchi.herokuapp.com/api/Pets/${params.id}/Scoldings`
+    )
+    if (response.status === 200) {
+      setPetScold(response.data)
+    }
+  }
 
-// useEffect(loadPetDetails, [ params.id ])
-console.log('setPetDetails', setPetDetails);
-console.log(params.id);
-console.log('petDetails', petDetails);
+  // useEffect(loadPetDetails, [ params.id ])
+  console.log('setPetDetails', setPetDetails)
+  console.log(params.id)
+  console.log('petDetails', petDetails)
 
-
-  return <div>
-    <h1>Pet Details
-    </h1>
-    <p>
-   pet id: {params.id!}
-    </p>
-    <p>
-    name: {petDetails.name}
-    hunger: {petDetails.hungerLevel}
-    </p>
-    <button onClick={deletePet}>Delete Pet</button>
-    <button onClick={feedPet}>{petHunger}</button>
-    <button onClick={playWithPet}>{petPlay}</button>
-    <button onClick={scoldPet}>{petScold}</button>
-  </div>
+  return (
+    <div>
+      <h1>Pet Details</h1>
+      <p>pet id: {params.id!}</p>
+      <p>
+        name: {petDetails.name}
+        hunger: {petDetails.hungerLevel}
+        happiness: {petDetails.happinessLevel}
+      </p>
+      <button onClick={deletePet}>Delete Pet</button>
+      <button onClick={feedPet}>Feed pet{petHunger}</button>
+      <button onClick={playWithPet}>Play with pet{petPlay}</button>
+      <button onClick={scoldPet}>Scold pet{petScold}</button>
+    </div>
+  )
 }
