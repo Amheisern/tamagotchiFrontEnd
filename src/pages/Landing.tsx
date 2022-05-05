b1a60afdca90f82b6b6572634b1d91504cf30e41
lodashreload <icarusjapan@gmail.com>
2022-05-05 00:01:34 -0400

    Css and small HTML changes

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -14,40 +14,43 @@ export function Landing() {
       )
       if (response.status === 200) {
         setPetList(response.data)
-        console.log(response.data);
-        
+        console.log(response.data)
       }
     }
-  fetchPets()
-}
-async function handleCreateNewPet() {
-  const response = await axios.post(
-    'https://lodashtamagotchi.herokuapp.com/api/Pets',
-    {   name: newPetName  }
-  )
-  if (response.status === 201) {
-    loadAllPets()
+    fetchPets()
   }
-}
-useEffect(loadAllPets, [])
-console.log('loadAllPets', loadAllPets)
-console.log('setPetList', setPetList)
+  async function handleCreateNewPet() {
+    const response = await axios.post(
+      'https://lodashtamagotchi.herokuapp.com/api/Pets',
+      { name: newPetName }
+    )
+    if (response.status === 201) {
+      loadAllPets()
+    }
+  }
+  useEffect(loadAllPets, [])
+  console.log('loadAllPets', loadAllPets)
+  console.log('setPetList', setPetList)
 
   return (
     <>
       <div>
-        <h1>List of Pets</h1>
+        <h2>List of Pets</h2>
         <div className="row">
           {petList.map(function (pet) {
             return (
               <>
                 <ul className="column">
-                <Link to={`./Pets/${pet.id}`}>
-                  <li className="card">{pet.name}</li>
-                </Link>
-                  <li className="card">{pet.birthday}</li>
-                  <li className="card">{pet.hungerLevel}</li>
-                  <li className='card'>{pet.happinessLevel}</li>
+                  <Link to={`./Pets/${pet.id}`}>
+                    <li className="card">{pet.name}</li>
+                  </Link>
+                  <li className="card">
+                    Birthday: ({new Date(pet.birthday).toLocaleDateString()})
+                  </li>
+                  <li className="card">Hunger Level: ({pet.hungerLevel})</li>
+                  <li className="card">
+                    Happiness Level: ({pet.happinessLevel})
+                  </li>
                 </ul>
               </>
             )
@@ -57,14 +60,16 @@ console.log('setPetList', setPetList)
           onSubmit={function (event) {
             event.preventDefault()
             handleCreateNewPet()
-          }}>
+          }}
+        >
           <input
             type="text"
             placeholder="Want to add a pet?"
             value={newPetName}
             onChange={function (event) {
               setNewPetName(event.target.value)
-            }} />
+            }}
+          />
         </form>
       </div>
     </>
